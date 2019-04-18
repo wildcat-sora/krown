6f4467b2399a50d1ea0bff0bdec223b2fab40c19
Daisuke-Ichikawa-2 <amethyst.rook@gmail.com>
2019-04-18 19:43:46 +0900

    4/18_ビュー修正
    4/18分コミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_084158) do
+ActiveRecord::Schema.define(version: 2019_04_18_101700) do
 
   create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_04_18_084158) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

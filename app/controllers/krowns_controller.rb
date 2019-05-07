0369f31581e5191062a740cd3894dce12447ec51
Daisuke-Ichikawa-2 <amethyst.rook@gmail.com>
2019-05-07 16:19:19 +0900

    ドロップダウン実装
    ドロップダウンにグルーピング記述

--- app/controllers/krowns_controller.rb
+++ app/controllers/krowns_controller.rb
@@ -18,17 +18,11 @@ class KrownsController < ApplicationController
   def edit
     @knowledge = Knowledge.new
     @max_id = Knowledge.maximum(:id) + 1
+    @genres = Genre.where(user_id: current_user.id.to_s)
   end
 
   def create
     @knowledge = Knowledge.new(params_knowledge)
-    if @knowledge.user_id == 0
-      @knowledge.user_id = 99
-    end
-    if @knowledge.genre_id == nil
-      @knowledge.genre_id = 1
-    end
-
     if @knowledge.save!
       redirect_to root_path, notice: 'ナレッジを作成しました'
     # else
@@ -47,6 +41,7 @@ private
     params.require(:knowledge).permit(
       :id,
       :user_id,
+      :genre_id,
       :title,
       :content,
       :keyword_1,

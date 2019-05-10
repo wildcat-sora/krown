488e8acba4b2a9ad00d6a4c24850a5ba18a06b19
Daisuke-Ichikawa-2 <amethyst.rook@gmail.com>
2019-05-10 21:21:51 +0900

    ヘッダ修正

--- app/controllers/krowns_controller.rb
+++ app/controllers/krowns_controller.rb
@@ -7,16 +7,15 @@ class KrownsController < ApplicationController
   # before_action @destroy_flg = false
 
   def index
-    # @knowledgebases = Knowledgebase.order("created_at DESC").page(params[:page]).per(5)
     # 最新ナレッジを10件最新
-    @knowledges = Knowledge.order("created_at DESC").limit(10)
+    @knowledges = Knowledge.page(params[:page]).per(12).order("created_at DESC")
     # 最新ナレッジのみを取得
     @setsql = Knowledge.order("created_at DESC").limit(1)
     @knowledge = @setsql[0]
   end
 
   def show
-     @knowledges = Knowledge.order("created_at DESC").limit(10)
+     @knowledges = Knowledge.page(params[:page]).per(12).order("created_at DESC")
      @knowledge = Knowledge.find(get_knowledge)
      @destroy_flg = true
   end
@@ -27,9 +26,12 @@ class KrownsController < ApplicationController
 
   def create
     @knowledge = Knowledge.new(params_knowledge)
-    binding.pry
-    @knowledge.img_flg = 1 unless @knowledge.image == ""
     if @knowledge.save!
+      image_check = Knowledge.find(@knowledge.id)
+      if image_check.image?
+        image_check.img_flg = 1
+        image_check.save
+      end
       redirect_to root_path, notice: 'ナレッジを作成しました'
     else
       render :new
@@ -78,10 +80,6 @@ private
 
   def get_max_id
     @max_id = Knowledge.maximum(:id) + 1
-  end
-
-  def up_img_flg
-
   end
 
 end

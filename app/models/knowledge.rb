f3702fa8ed331237617b652420eb73144ae591ba
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-06-10 10:06:25 +0900

    2.2_001 バリデーションの実装
    gitがおかしくなったため、精査する。

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -3,9 +3,9 @@ class Knowledge < ApplicationRecord
     belongs_to :genre
     mount_uploader :image, ImageUploader
 
-
-
-
-
+    validates :id, presence: { message: "が入力されていません。"}
+    validates :id, numericality: { message: "数字で入力してください。"}
+    validates :title, presence: { message: "が入力されていません。"}
+    validates :content, presence: { message: "が入力されていません。"}
 
 end

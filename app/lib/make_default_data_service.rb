65ff7be4b2c0b52caf6a4f5092b5d518867298d6
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-06-12 18:00:25 +0900

    #003:初期データ挿入処理見直し
    yamlファイルにてデータインサートする

--- app/lib/make_default_data_service.rb
+++ app/lib/make_default_data_service.rb
@@ -1,4 +1,5 @@
 class MakeDefaultDataService
+  require('yaml')
 
   def initialize(mode:)
     case mode
@@ -10,22 +11,46 @@ class MakeDefaultDataService
   end
 
   def make_default_data
-    # ユーザ未登録でも同アプリが使用出来るようにデフォルトユーザを作成する
-    default_user = User.new
-    default_user.update_attributes(
-      id: 1,
-      password: 111111,
-      name: '未登録ユーザ',
-      email: 'example@test.com'
-    )
+    ActiveRecord::Base.transaction do
 
-    # ユーザ未登録でも同アプリが使用出来るようにデフォルトユーザを作成する
-    default_genre = Genre.new
-    default_genre.update_attributes(
-      id: 1,
-      user_id:  1,
-      in_genre: 'ジャンル未登録'
-      )
+      ## ユーザ未登録でも同アプリが使用出来るようにデフォルトユーザを作成する
+      insert_user_data()
+
+      ## ユーザ未登録でも同アプリが使用出来るようにデフォルトユーザを作成する
+      insert_genre_data()
+
+    end
+
   end
+
+  def insert_user_data
+    begin
+      default_user = User.new
+      insert_data = YAML.load_file('app/lib/yaml/default_user.yml')
+      insert_data.each do | data |
+        default_user.id = data["id"]
+        default_user.password = data["password"]
+        default_user.name = data["name"]
+        default_user.email = data["email"]
+      end
+
+      default_user.save
+    end
+  end
+
+  def insert_genre_data
+    begin
+      default_genre = Genre.new
+      insert_data = YAML.load_file('app/lib/yaml/default_genre.yml')
+      insert_data.each do | data |
+        default_genre.id = data["id"]
+        default_genre.user_id = data["user_id"]
+        default_genre.in_genre = data["in_genre"]
+      end
+
+      default_genre.save
+    end
+  end
+
 
 end

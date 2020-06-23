da988d49db404e2086249d26ed42b474d3a15f23
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-06-23 17:32:00 +0900

    #101:表示項目数見直し

--- app/helpers/krowns_helper.rb
+++ app/helpers/krowns_helper.rb
@@ -5,14 +5,14 @@ module KrownsHelper
   def search_knowledge_data(page: nil)
     # データ表示上限を100件とする
     tmp_data = Knowledge.order(user_id: :asc,created_at: :desc,id: :desc).limit(100)
-    data = tmp_data.page(page).per(10)
+    data = tmp_data.page(page).per(13)
 
     data
   end
 
   def search_result_knowledge_data(keyword: nil, page: nil)
     tmp_data = Knowledge.where('title LIKE(?) OR content LIKE(?)' ,"%#{keyword}%","%#{keyword}%").order(created_at: :desc)
-    data = tmp_data.page(page).per(10)
+    data = tmp_data.page(page).per(13)
 
     data
   end

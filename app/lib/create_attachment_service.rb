06cdb81edddd72eccff68a0b601ea66802e36bd7
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-07-02 11:27:40 +0900

    #102:添付ファイル機能追加

---
+++ app/lib/create_attachment_service.rb
@@ -0,0 +1,33 @@
+# イメージデータ作成サービス
+
+class CreateAttachmentService
+
+  def initialize
+  end
+
+  def attachment_data_create(image_data)
+    attachment = Attachment.new()
+
+    # イメージpath挿入
+    attachment.image = image_data
+    #イメージファイルのファイル名
+    attachment.name = attachment.get_name
+    #ファイルのタイプを取得する
+    attachment.file_type = attachment.get_type
+
+    #イメージファイルのサブid（【仮】現時点では添付ファイルは１枚のみで運用-固定）
+    attachment.sub_id = 1
+    # ファイルサイズ挿入
+    attachment.file_size = attachment.get_file_size
+    #それぞれファイルのurlを設定しておく
+    attachment.image_url = image_data.url
+    attachment.thumb_image_url = image_data.thumb.url
+
+    #各イメージファイルの大きさを取得
+    attachment.width_size = attachment.get_image_size[0]
+    attachment.height_size = attachment.get_image_size[1]
+
+    attachment
+  end
+
+end
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,2 +1,30 @@
 class Attachment < ApplicationRecord
+  belongs_to :knowledge
+  mount_uploader :image, ImageUploader
+
+  def public_path
+    @public_path ||= "public".freeze
+  end
+
+  #ファイルサイズを取得する
+  def get_file_size
+    stat = File.stat(File.join(public_path,self.image.url))
+    stat.size
+  end
+
+  #ファイル名を取得する
+  def get_name
+    File.basename(File.join(public_path,self.image.url))
+  end
+
+  #イメージファイルの大きさを取得する
+  def get_image_size
+    image_size = FastImage.size(File.join(public_path,self.image.url))
+  end
+
+  #イメージファイルのタイプを取得する
+  def get_type
+    image_size = FastImage.type(File.join(public_path,self.image.url))
+  end
+
 end

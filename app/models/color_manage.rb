35c78840c5e36cbe9a94b09a1d2c33dd76046fb5
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-07-07 18:51:58 +0900

    #102:アソシエーション設定

--- app/models/color_manage.rb
+++ app/models/color_manage.rb
@@ -1,4 +1,5 @@
 class ColorManage < ApplicationRecord
   enum color_type: { single: 0, double: 1, graphic: 2 }
-  belongs_to :knowledge
+  belongs_to  :knowledge
+  accepts_nested_attributes_for :knowledge
 end

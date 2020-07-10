404901e40b7b67126262ab5c167689acf978cbab
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-07-10 10:29:09 +0900

    #101:添付画像、全角対応

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,5 @@
 class Attachment < ApplicationRecord
+  include ApplicationHelper
   belongs_to :knowledge
   mount_uploader :image, ImageUploader
 
@@ -8,10 +9,14 @@ class Attachment < ApplicationRecord
     @public_path ||= "public".freeze
   end
 
+  def attachment_file_path
+    attachment_file_path ||= make_path(File.dirname(self.image.url),self.image.filename)
+  end
+
   #ファイルサイズを取得する
   def get_file_size
     if self.image.url
-      stat = File.stat(File.join(public_path,self.image.url))
+      stat = File.stat(File.join(public_path,attachment_file_path)) if self.image.url
       stat.size
     end
 
@@ -19,28 +24,28 @@ class Attachment < ApplicationRecord
 
   #ファイル名を取得する
   def get_name
-      File.basename(File.join(public_path,self.image.url)) if self.image.url
+      File.basename(File.join(public_path,attachment_file_path)) if self.image.url
   end
 
   #イメージファイルの大きさを取得する
   def get_image_size
-    image_size = FastImage.size(File.join(public_path,self.image.url)) if self.image.url
+    image_size = FastImage.size(File.join(public_path,attachment_file_path)) if self.image.url
   end
 
   #イメージファイルのタイプを取得する
   def get_type
-    image_size = FastImage.type(File.join(public_path,self.image.url)) if self.image.url
+    image_size = FastImage.type(File.join(public_path,attachment_file_path)) if self.image.url
   end
 
   #添付されたファイルが以下の種類以外は保存できない。
   # 許容ファイル属性：png,jpg,gif
   def file_type_check
-    #if file_type.in?(['png','jpg','gif','jpeg'])
-    #  true
-    #else
-    #  errors.add(:file_type , "指定のファイル属性を添付してください。")
-    #  false
-    #end
+    if file_type.in?(['png','jpg','gif','jpeg'])
+      true
+    else
+      errors.add(:file_type , "指定のファイル属性を添付してください。")
+      false
+    end
 
   end
 

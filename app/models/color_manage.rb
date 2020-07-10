90f450f8878a21c35fe54515b8f7313fd42ce413
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-07-10 23:00:39 +0900

    #101:モデル編集

--- app/models/color_manage.rb
+++ app/models/color_manage.rb
@@ -1,5 +1,4 @@
 class ColorManage < ApplicationRecord
   enum color_type: { single: "single", double: "double", graphic: "graphic" }
   belongs_to  :knowledge
-  accepts_nested_attributes_for :knowledge
 end
--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -2,8 +2,10 @@ class Knowledge < ApplicationRecord
     belongs_to :user
     belongs_to :genre
     has_many   :attachments, dependent: :destroy
-    has_many   :color_manage,  dependent: :destroy
+    has_many   :color_manages,  dependent: :destroy
     mount_uploader :image, ImageUploader
+    accepts_nested_attributes_for :attachments
+    accepts_nested_attributes_for :color_manages
 
     validates :id, presence: { message: "が入力されていません。"}
     validates :id, numericality: { message: "数字で入力してください。"}

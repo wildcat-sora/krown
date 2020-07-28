570c6203b46a5aabf3b93946e07aee06cb6d068d
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-07-28 17:58:02 +0900

    #101:color_group追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_062940) do
+ActiveRecord::Schema.define(version: 2020_07_28_083119) do
 
   create_table "attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "knowledge_id"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_07_28_062940) do
     t.string "word_color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "group_flg"
+    t.string "parent_id"
     t.index ["knowledge_id"], name: "index_color_manages_on_knowledge_id"
   end
 

e0349a2ec2764b195ae1a0f03130bebd982ef6ee
Daisuke Ichikawa <ichikawa_daisuke2@gmail.com>
2020-07-29 12:32:33 +0900

    #102:リレーション変更に伴うモデル変更

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -3,10 +3,10 @@ class Knowledge < ApplicationRecord
     belongs_to :user
     belongs_to :genre
     has_many   :attachments, dependent: :destroy
-    has_many   :color_manages,  dependent: :destroy
+    belongs_to   :color_manage
+
     mount_uploader :image, ImageUploader
     accepts_nested_attributes_for :attachments
-    accepts_nested_attributes_for :color_manages
 
     validates :id, presence: { message: "が入力されていません。"}
     validates :id, numericality: { message: "数字で入力してください。"}
